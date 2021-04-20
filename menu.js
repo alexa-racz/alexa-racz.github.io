574af3ffba2ea103436aba45779da551e3db19ad
alexa-racz <78875398+alexa-racz@users.noreply.github.com>
2021-04-20 19:06:57 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,15 @@
 var menu = document.getElementById("menu");
+var button = document.getElementById("button");
 var savedZIndex = menu.style.zIndex;
 var savedPosition = menu.style.position;
 var savedBackgroundColor = menu.style.backgroundColor;
+
 function toggleMenu() {
-    menu.style.display = menu.style.display == "none" ? "flex" : "none";
+    menu.style.display = menu.style.display != "flex" ? "flex" : "none";
     menu.style.zIndex++;
     menu.style.position = "absolute";
+    menu.style.left = "0";
+    menu.style.top = (button.offsetTop + button.offsetHeight) + "px";
     menu.style.backgroundColor = "#640c0c"; // rgb(100, 12, 12);
     menu.style.width = "100%";
 }
